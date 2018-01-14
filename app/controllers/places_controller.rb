9bc6a2c326630efe027684b2211cd11d864b0de7
Barry Bradley <bbradley13@gmail.com>
2018-01-14 19:41:52 +0000

    Made changes to user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,8 @@
   class PlacesController < ApplicationController
-
+        #Allows a before filter which only lets logged in users
+        #access 'new' and 'create'. Users not logged in will be directed
+        #to the login in page when clicking on 'new place'
+      before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
@@ -7,9 +10,12 @@
   def new
     @place = Place.new
   end
-  
+  #Runs when the create button pressed on the form
   def create
-      Place.create(place_params)
+      #Place.create(place_params)
+      #Creates a place connected with the user. Knows this bc of has_many :places
+      #in the user.rb file
+      current_user.places.create(place_params)
       redirect_to root_path
   end
 
