31f888446b68ce54e3088eea8debbd6fa2c0cb55
Barry Bradley <bbradley13@gmail.com>
2018-01-19 03:55:06 +0000

    Editing comments form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,17 @@ class Comment < ApplicationRecord
 #comments point to user and place, other tables in the db
   belongs_to :user
   belongs_to :place
+
+    RATINGS = {
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
+#Calls this method for all comments
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
+
 end
