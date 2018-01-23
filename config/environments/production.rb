a30c548f568fc251660cf5b23946776edba962fc
Barry Bradley <bbradley13@gmail.com>
2018-01-23 02:07:18 +0000

    Configuring mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'https://nomster-barry-bradley.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'nomster-barry-bradley.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
