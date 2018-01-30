af8d27a1956f891e3dde66c06438547d6a732b02
Barry Bradley <bbradley13@gmail.com>
2018-01-30 02:25:20 +0000

    Adding user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-
+#takes the id from the URL. looks up user in database and puts value in user variable
   def show
-
+    @user = User.find(params[:id])
+    
   end
 end
