5ea58da2b2f9bc09a1b35f713890acd95f1e6fd6
Barry Bradley <bbradley13@gmail.com>
2018-02-02 02:40:23 +0000

    Changed number of places per page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@
         #to the login in page when clicking on 'new place'
       before_action :authenticate_user!, only: [:new, :create]
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 1)
+    @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
